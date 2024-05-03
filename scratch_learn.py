fe5177f2c389ef8b4e0bbb455ed7dd6836668333
marichandrant <149341179+marichandrant@users.noreply.github.com>
2024-05-02 22:11:20 -0500

    Update scratch_learn.py

--- scratch_learn.py
+++ scratch_learn.py
@@ -2,3 +2,6 @@ from learn.oops.common_module import emp_sal_management
 doubleclick_load_in_ram_object1=emp_sal_management()#load class in memory by giving ()
 print(doubleclick_load_in_ram_object1.sal_bon(20000,1000))
 print(doubleclick_load_in_ram_object1.sal_bon_inc(20000,1000,500))
+
+
+--new
